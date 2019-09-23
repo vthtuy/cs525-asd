37e6b21f6d50dd361d3fb4d1450ca84eba4b0ef7
TuyVo <vthtuy2@gmail.com>
2019-09-23 00:26:24 -0700

    lab12 -part b, config bean & injection using classpath scanning instead of xml configuration. @Service, @Repository, @Controller, component-scan, context:annotation-config, @Component

---
+++ src/lab12_spring/Lab12SimpleSpring_b/src/main/java/customers/CustomerDAO.java
@@ -0,0 +1,20 @@
+package customers;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerDAO implements ICustomerDAO{
+	private ILogger logger = new Logger();
+	
+	public void save(Customer customer) {
+		// simple sleep
+		try {
+			Thread.sleep(350);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("CustomerDAO: saving customer "+customer.getName());
+		logger.log("Customer is saved in the DB: "+ customer.getName() );
+	}
+
+}
